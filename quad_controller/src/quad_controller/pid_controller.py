57924ed2d7e3f5b1e5a2b118c08515aa07325959
jjjjohnson <jjjjohnsonjin@gmail.com>
2017-08-23 16:10:07 +0800

    Fill in function for p i d

--- quad_controller/src/quad_controller/pid_controller.py
+++ quad_controller/src/quad_controller/pid_controller.py
@@ -7,33 +7,75 @@
 class PIDController:
     def __init__(self, kp = 0.0, ki = 0.0, kd = 0.0, max_windup = 10):
         #TODO
-        pass
-    def reset(self):
-        #TODO
-        pass
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.max_windup = max_windup
 
+        #store relavent data
+        self.last_timestamp = 0.0
+        self.set_point = 0.0
+        self.error_sum = 0.0
+        self.last_error = 0.0
+        self.alpha = 0.2
+    def reset(self):
+        self.kp = 0.0
+        self.ki = 0.0
+        self.kd = 0.0
+        
+
+        #store relavent data
+        self.last_timestamp = 0.0
+        self.set_point = 0.0
+        self.error_sum = 0.0
+        self.last_error = 0.0
     def setTarget(self, target):
-        #TODO
-        pass
+        self.set_point = target
 
     def setKP(self, kp):
-        #TODO
-        pass
+        self.kp = kp
 
     def setKI(self, ki):
-        #TODO
-        pass
+        self.ki = ki
 
     def setKD(self, kd):
-        #TODO
-        pass
+        self.kd = kd
 
     def setMaxWindup(self, max_windup):
-        #TODO
-        pass
+        self.max_windup = int(max_windup)
 
     def update(self, measured_value, timestamp):
-        #TODO
-        pass
+        delta_time = timestamp - self.last_timestamp
+        if delta_time == 0:
+        	return 0
+
+        error = self.set_point - measured_value
+
+        self.last_timestamp = timestamp
+
+        self.error_sum = error*delta_time
+
+        delta_error = error - self.last_error
+
+        # Update past error
+        self.last_error = error
+
+        # Address max windup
+        if self.error_sum > self.max_windup:
+        	self.error_sum = self.max_windup
+        elif self.error_sum < -self.max_windup:
+        	self.error_sum = -self.max_windup
+
+        p = self.kp*error
+        i = self.ki*self.error_sum
+
+        # incorporating derivative smoothing
+        d = self.kd*(self.alpha * delta_error / delta_time + (1-self.alpha))
+
+        u = p + i + d
+
+        print("u is: ", u)
+
+        return -u
 
 
